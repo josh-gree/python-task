9030abac42cc0898707a3cb70c5b46b64e975412
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2019-06-21 19:04:36 +0200

    add simple setup.py

---
+++ scraper/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="scraper",
+    version="0.1",
+    description="Module for scraping adverts from heyjobs",
+    author="Joshua Greenhalgh",
+    packages=["scraper"],
+    zip_safe=False,
+)
